748acf891e48c5e2e0cd946777ea51abb47ccecc
QuentinHacourt <quentin.hacourt@gmail.com>
2024-07-16 15:29:35 +0200

    can now count voters and sum them up per story

--- script.py
+++ script.py
@@ -1,17 +1,44 @@
 import csv
+
 # make a set of all stories, this prevents duplicates
 stories = set()
+#
+story_entries = []
 
 # opens the input file
 with open('exercise_input.csv', 'r') as input_file:
     # reads the csv file and
     # puts the entries in a list of dictionaries
-    input = csv.DictReader(input_file)
+    input = list(csv.DictReader(input_file))
 
     # iterates over each entry in the file
     for entry in input:
-        # adds the story names from the
-        # "User story 1" column to the list of stories
-        stories.add(entry['User story 1'])
+        # adds the story names from all the "User story" columns
+        stories.update({
+            entry['User story 1'],
+            entry['User story 2'],
+            entry['User story 3'],
+            entry['User story 4'],
+            entry['User story 5'],
+        })
 
-print(stories)
+    for story in stories:
+        story_entry = {
+            "title": story,
+            "voters": set(),
+            "vote-count": 0
+        }
+        story_entries.append(story_entry)
+
+    for entry in input:
+        for story in story_entries:
+            if (entry['User story 1'] == story['title'] or
+                entry['User story 2'] == story['title'] or
+                entry['User story 3'] == story['title'] or
+                entry['User story 4'] == story['title'] or
+                entry['User story 5'] == story['title']):
+                story['vote-count'] += 1
+                story['voters'].add(entry['Your name'])
+
+
+print(story_entries)
