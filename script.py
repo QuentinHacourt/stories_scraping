97001e9551d54165ca315849d481df9ef603790a
QuentinHacourt <quentin.hacourt@gmail.com>
2024-07-16 14:31:03 +0200

    iterate over each entry

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
+import csv
+# make a set of all stories, this prevents duplicates
 stories = set()
+
+# opens the input file
+with open('exercise_input.csv', 'r') as input_file:
+    # reads the csv file and
+    # puts the entries in a list of dictionaries
+    input = csv.DictReader(input_file)
+
+    # iterates over each entry in the file
+    for entry in input:
+        print(entry)
