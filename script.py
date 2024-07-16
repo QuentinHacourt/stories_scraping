bd38b9cf90de164c84642b6a7c5ead5bb7070fa3
QuentinHacourt <quentin.hacourt@gmail.com>
2024-07-16 15:45:52 +0200

    works

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import csv
 
 # make a set of all stories, this prevents duplicates
 stories = set()
-#
+# list of story entries
 story_entries = []
 
 # opens the input file
@@ -22,14 +22,16 @@ with open('exercise_input.csv', 'r') as input_file:
             entry['User story 5'],
         })
 
+    # make dictionaries out of unique story titles
+    # for now every story has an empty set of voters
     for story in stories:
         story_entry = {
             "title": story,
             "voters": set(),
-            "vote-count": 0
         }
         story_entries.append(story_entry)
 
+    # add names to list of voter of entries if they voted for this entry
     for entry in input:
         for story in story_entries:
             if (entry['User story 1'] == story['title'] or
@@ -37,8 +39,11 @@ with open('exercise_input.csv', 'r') as input_file:
                 entry['User story 3'] == story['title'] or
                 entry['User story 4'] == story['title'] or
                 entry['User story 5'] == story['title']):
-                story['vote-count'] += 1
                 story['voters'].add(entry['Your name'])
 
+# sort the entries (from most votes to least votes)
+sorted_story_entries = sorted(story_entries, key=lambda d: len(d['voters']), reverse=True)
 
-print(story_entries)
+# pretty print all entries one by one
+for story_entry in sorted_story_entries:
+    print("title: {}, voters: {}, voter-count: {}".format(story_entry['title'], ", ".join(story_entry['voters']), len(story_entry['voters'])))
